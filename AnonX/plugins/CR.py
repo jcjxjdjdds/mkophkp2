cf619d363456f512c3346025f5e2a049aeeaa8c2
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2023-11-26 22:15:52 +0200

    Update CR.py

--- AnonX/plugins/CR.py
+++ AnonX/plugins/CR.py
@@ -20,25 +20,25 @@ from random import  choice, randint
 )
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/520cb8756d31bb3184de2.jpg",
+        photo=f"https://telegra.ph/file/679986673c48326b4ddb2.jpg",
         caption=f"""**⩹━★⊷━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺**\nمرحبا بك عزيزي {message.from_user.mention} في قسم مطورين cr ميوزك\nللتحدث مع مطورين اضغط علي الازرار بالاسفل👇\n**⩹━★⊷━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "᳒Monzer مــنــذرَّّ ", url=f"https://t.me/O_F_4"), 
+                        "᳒مــطور السـورس  ", url=f"https://t.me/o_f_4"), 
                  ],[
                     InlineKeyboardButton(
-                        "𓆩𝗠𝗔𝗡𝗗𝗢𝗢𖠲𓆪", url=f"https://t.me/M_S_2A"),
+                        "قـناه السـورس ", url=f"https://t.me/NO_VP8"),
                 ],[
                     InlineKeyboardButton(
-                        "༤ 𝗧𝗜𝗟𝗔𝗡𝗢༒ 𝗞𝗘𝗘𝗣𝗘𝗥 ༒ 𝗜𝗦𝗠𝗔𝗟𝗜𝗔", url=f"https://t.me/TT_TXI"),
+                        "قناه بوستات", url=f"https://t.me/N_8888_o"),
                     InlineKeyboardButton(
-                        "𝐄3𝐃𝐀𝐌", url=f"https://t.me/DAD_E3DAM"),
+                        "قـناه اكواد", url=f"https://t.me/noordot"),
                 ],[
                 
                     InlineKeyboardButton(
-                        "★⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝⚡", url=f"https://t.me/TNT_source"),
+                        "★⌞ قناه السورس ⌝⚡", url=f"https://t.me/NO_VP8"),
                 ],
 
             ]
@@ -55,12 +55,12 @@ async def huhh(client: Client, message: Message):
 
 
 @app.on_message(
-    command(["كريم","منذر","منزر","كيمو","monzer","باشا"])
+    command(["نور"])
     & filters.group
     & ~filters.edited
 )
 async def yas(client, message):
-    usr = await client.get_chat("O_F_4")
+    usr = await client.get_chat("o_f_4")
     name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
     await message.reply_photo(photo,       caption=f"**⩹━★⊷━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺\n\n‍ ¦ᦔꫀꪜ :{name}\n ¦ꪊ𝘴ꫀ𝘳 :@{usr.username}\n ¦Ꭵժ :`{usr.id}`\n ¦ႦᎥ᥆ :{usr.bio}\n\n**⩹━★⊷━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺**", 
@@ -76,12 +76,12 @@ async def yas(client, message):
 
 
 @app.on_message(
-    command(["اعدام","اعدومتي","اعدام","عدام باشا","Adam","اخو منذر"])
+    command(["عفرتو"])
     & filters.group
     & ~filters.edited
 )
 async def yas(client, message):
-    usr = await client.get_chat("devpokemon")
+    usr = await client.get_chat("x77_3")
     name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
     await message.reply_photo(photo,       caption=f"**⩹━★⊷━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺\n\n¦ᦔꫀꪜ :{name}\n ¦ꪊ𝘴ꫀ𝘳 :@{usr.username}\n ¦Ꭵժ :`{usr.id}`\n ¦ႦᎥ᥆ :{usr.bio}\n\n**⩹━★⊷━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺**", 
@@ -97,12 +97,12 @@ async def yas(client, message):
 
 
 @app.on_message(
-    command(["تيلانو","طلايانو","شق منذر","العلق","تيلانو","تيلانو"])
+    command(["حمو","حمو ","شق حمو ","العلق","حمو","حمو"])
     & filters.group
     & ~filters.edited
 )
 async def yas(client, message):
-    usr = await client.get_chat("dr_criss")
+    usr = await client.get_chat("T4_Mohamed")
     name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
     await message.reply_photo(photo,       caption=f"**⩹━★⊷━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺\n\n‍ ¦ᦔꫀꪜ :{name}\n ¦ꪊ𝘴ꫀ𝘳 :@{usr.username}\n ¦Ꭵժ :`{usr.id}`\n ¦ႦᎥ᥆ :{usr.bio}\n\n**⩹━★⊷━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺**", 
@@ -118,12 +118,12 @@ async def yas(client, message):
     
 
 @app.on_message(
-    command(["نور"])
+    command(["المطور","الحاكم ","نور","noor","noor"])
     & filters.group
     & ~filters.edited
 )
 async def yas(client, message):
-    usr = await client.get_chat("M_S_2A")
+    usr = await client.get_chat("O_F_4")
     name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
     await message.reply_photo(photo,       caption=f"**⩹━★⊷━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺\n\n ¦ᦔꫀꪜ :{name}\n ¦ꪊ𝘴ꫀ𝘳 :@{usr.username}\n ¦Ꭵժ :`{usr.id}`\n ¦ႦᎥ᥆ :{usr.bio}\n\n**⩹━★⊷━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━⊶★━⩺**", 
@@ -146,17 +146,17 @@ async def yas(client, message):
 )
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/520cb8756d31bb3184de2.jpg",
+        photo=f"https://telegra.ph/file/679986673c48326b4ddb2.jpg",
         caption=f"""**⩹⊷━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━━⩺**\nمرحبا بك عزيزي {message.from_user.mention} في قسم الذكاء الاصتناعي الخاص بسورس cr\nلتتمكن من استخدام اوامر الذكاء الاصتناعي اكتب \n /gpt + السؤال بالاسفل👇\n**⩹━━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━━⩺**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "᳒مطـور السـورسَّّ", url=f"https://t.me/O_F_4"), 
+                        "᳒مــطور السـورس ", url=f"https://t.me/o_f_4"), 
                  ],[
                 
                     InlineKeyboardButton(
-                        "★⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝⚡", url=f"https://t.me/O_F_4"),
+                        "★⌞  قناه السورس ⌝⚡", url=f"https://t.me/NO_VP8"),
                 ],
 
             ]
@@ -174,17 +174,17 @@ async def huhh(client: Client, message: Message):
 )
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/520cb8756d31bb3184de2.jpg",
+        photo=f"https://telegra.ph/file/679986673c48326b4ddb2.jpg",
         caption=f"""**⩹⊷━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━━⩺**\nمرحبا بك عزيزي {message.from_user.mention} في قسم تشغيل القرأن الخاص بسورس cr\nلتتمكن من استخدام اوامر القرأن اكتب \n سورة + اسم السورة بالاسفل👇\n**⩹━━⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝━━⩺**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "᳒مطـور السـورس", url=f"https://t.me/O_F_4"), 
+                        "᳒مــطور السـورس ", url=f"https://t.me/o_f_4"), 
                  ],[
                 
                     InlineKeyboardButton(
-                        "★⌞  𝗡𝗢𝗢𝗥 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝⚡", url=f"https://t.me/O_F_4"),
+                        "★⌞  قناه السورس ⌝⚡", url=f"https://t.me/NO_VP8"),
                 ],
 
             ]
